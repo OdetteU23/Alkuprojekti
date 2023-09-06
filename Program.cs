9877ef84284e476a789f5bbab62d1d763136c989
Kati Lehtonen <125460153+Dezaiasu@users.noreply.github.com>
2023-09-06 15:09:30 +0300

    random ohjelma lisätty

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace harjoitus1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kuinka monta lukua?");
+            int luvut = Convert.ToInt32(Console.ReadLine());
+
+            double[] numerot = new double[luvut];
+
+            for (int i = 0; i < luvut; i++)
+            {
+                Console.WriteLine("Anna luku " + (i + 1) + ": ");
+                numerot[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double summa = 0;
+            foreach (double num in numerot)
+            {
+                summa += num;
+            }
+
+            double keskiarvo = summa / luvut;
+
+            Console.WriteLine("Lukuja oli: " + luvut);
+            Console.WriteLine("Lukujen keskiarvo on: " + keskiarvo.ToString());
+            Console.WriteLine("Syötetyt luvut olivat: ");
+            foreach (double num in numerot)
+            {
+                Console.WriteLine(num.ToString());
+            }
+        }
+    }
+}
